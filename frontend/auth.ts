66ec5ed5467e29d11095fd327bac829123dc8baf
조현창 <gusckd57@naver.com>
2025-11-17 02:08:22 +0900

    feat: 프론트엔드 인증로직 추가

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,10 +1,29 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    useSecureCookies: process.env.NODE_ENV === "production",
+    secret: process.env.AUTH_SECRET,
     providers: [
-
-
-
-
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "이메일을 입력해주세요" },
+                password: { label: "Password", type: "password", placeholder: "비밀번호를 입력해주세요" },
+            },
+            async authorize(credentials) {
+                const user = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+                    method: "POST",
+                    body: JSON.stringify(credentials),
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                });
+                if (!user.ok) {
+                    throw new Error("Failed to login");
+                }
+                return user.json();
+            },
+        }),
     ],
 });
