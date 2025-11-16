e81201946627f28a211f84fa2565c263a63ce64c
조현창 <gusckd57@naver.com>
2025-11-17 01:04:09 +0900

    feat(frontend): 상태관리 추가

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Provider } from "jotai";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider>
+        <QueryClientProvider client={queryClient}>
+      {children}
+      </QueryClientProvider>
+    </Provider>
+  );
+}
